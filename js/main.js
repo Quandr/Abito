3fb46dea6fefe038cb5fd9ff1406f5c4baa30c43
KuznetsovAA <KuznetsovAA@titul2005.ru>
2021-07-30 10:37:03 +0400

    День четвертый

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 10,
-  slidesPerView: 6,
-  freeMode: true,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".mySwiper2", {
-  spaceBetween: 10,
-  thumbs: {
-    swiper: swiper,
-  },
-});
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-panel");
+const menuCloseBtn = document.querySelector(".close-menu");
+const toggleIsOpen = function () {
+  menu.classList.toggle("is-open");
+};
+menuBtn.addEventListener("click", toggleIsOpen);
+menuCloseBtn.addEventListener("click", toggleIsOpen);
